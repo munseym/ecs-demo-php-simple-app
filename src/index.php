0c65349c81b87f15fcfcf0ae6178d53a2fb9d2be
munseym <munsey@gmail.com>
2017-06-05 20:50:54 -0700

    Update index.php

--- src/index.php
+++ src/index.php
@@ -15,8 +15,8 @@
     <body>
         <div class="container">
             <div class="hero-unit">
-                <h1>Simple PHP App</h1>
-                <h2>Congratulations</h2>
+                <h1>Michael Munsey</h1>
+                <h2>CLDAWS 230 B Sp 17: System Operations For Amazon Web Services</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
